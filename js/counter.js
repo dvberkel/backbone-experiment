b384d7cce53f26feb5789da40a919a137fee9d26
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-01-26 19:39:09 +0100

    [REFACTOR] order of creation
    Some code can already be defined before the document finished loading.

--- js/counter.js
+++ js/counter.js
@@ -1,43 +1,42 @@
 (function($){
-    $(function(){
-	window.CounterModel = Backbone.Model.extend({
-	    defaults: {
-		value: 0
-	    },
-	    
-	    increase : function() {
-		this._changeBy(1);
-	    },
-	    
-	    _changeBy : function(amount) {
-		amount = amount || 1;
-		this.set({value: this.get("value") + amount});
-	    },
-	    
-	    decrease : function() {
-		this._changeBy(-1);
-	    }
-	});
+    window.CounterModel = Backbone.Model.extend({
+	defaults: {
+	    value: 0
+	},
+	
+	increase : function() {
+	    this._changeBy(1);
+	},
+	
+	_changeBy : function(amount) {
+	    amount = amount || 1;
+	    this.set({value: this.get("value") + amount});
+	},
+	
+	decrease : function() {
+	    this._changeBy(-1);
+	}
+    });
 
-	window.CounterView = Backbone.View.extend({
-	    el: $("#counter"),
+    window.CounterView = Backbone.View.extend({
+	initialize: function(){
+	    _.bindAll(this, 'render');
 	    
-	    initialize: function(){
-		_.bindAll(this, 'render');
-		
-		this.model.bind("change:value", function(){
-		    this.render();
-		}, this);
+	    this.model.bind("change:value", function(){
 		this.render();
-	    },
-	    
-	    render: function(){
-		$(this.el).text(this.model.get("value"));
-	    }
-	});
+	    }, this);
+	    this.render();
+	},
+	
+	render: function(){
+	    $(this.el).text(this.model.get("value"));
+	}
+    });
+
+    $(function(){
 	
 	var counterModel = new CounterModel();
-	var counterView = new CounterView({model: counterModel});
+	var counterView = new CounterView({el: $("#counter"), model: counterModel});
 	
 	$("#increase").click(function(){
 	    counterModel.increase();
